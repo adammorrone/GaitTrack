bed6a58ac0335434ad233734b0ee7fc7aaa625a2
adammorrone <adammorrone2@gmail.com>
2019-05-06 21:59:51 -0600

    added abilityt to track red and yellow at the same time, graphs x pos of each afterwards

--- GaitTrack.py
+++ GaitTrack.py
@@ -6,6 +6,7 @@ import argparse
 import cv2
 import imutils
 import time
+import matplotlib.pyplot as plt
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -18,9 +19,14 @@ args = vars(ap.parse_args())
 # define the  lower and upper boundaries of the "green"
 # ball in the HSV color space, then initialize the
 # list of tracked points
-yellowLower = (29, 86, 6)
-yellowUpper = (64, 255, 255)
-pts = deque(maxlen=args["buffer"])
+yellowLower = (28, 48, 85)
+yellowUpper = (62, 182, 255)
+redLower = (118, 80, 96)
+redUpper = (205, 201, 234)
+yellow_pts = deque(maxlen=args["buffer"])
+red_pts = deque(maxlen=args["buffer"])
+red_data = np.array([])
+yellow_data = np.array([])
 
 # if a video path was not supplied, grab the reference
 # to the webcam
@@ -49,7 +55,7 @@ while True:
 
     # resize the frame, blur it, and convert it to the HSV
     # color space
-    #frame = imutils.resize(frame, width=1080)
+    frame = imutils.resize(frame, width=1400)
 
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
@@ -57,49 +63,93 @@ while True:
     # construct a mask for the color "green", then perform
     # a series of dilations and erosions to remove any small
     # blobs left in the mask
-    mask = cv2.inRange(hsv, yellowLower, yellowUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+    yellow_mask = cv2.inRange(hsv, yellowLower, yellowUpper)
+    red_mask = cv2.inRange(hsv, redLower, redUpper)
+
+    yellow_mask = cv2.erode(yellow_mask, None, iterations=2)
+    yellow_mask = cv2.dilate(yellow_mask, None, iterations=2)
+
+    red_mask = cv2.erode(red_mask, None, iterations=2)
+    red_mask = cv2.dilate(red_mask, None, iterations=2)
 
     # find contours in the mask and initialize the current
     # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+    yellow_cnts = cv2.findContours(yellow_mask.copy(), cv2.RETR_EXTERNAL,
                             cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
-    center = None
+    yellow_cnts = imutils.grab_contours(yellow_cnts)
+    yellow_center = None
 
     # only proceed if at least one contour was found
-    if len(cnts) > 0:
+    if len(yellow_cnts) > 0:
         # find the largest contour in the mask, then use
         # it to compute the minimum enclosing circle and
         # centroid
-        c = max(cnts, key=cv2.contourArea)
+        c = max(yellow_cnts, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        yellow_center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
         # only proceed if the radius meets a minimum size
-        if radius > 10:
+        if radius > 8:
             # draw the circle and centroid on the frame,
             # then update the list of tracked points
             cv2.circle(frame, (int(x), int(y)), int(radius),
-                       (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                       (255, 0, 0), 2)
+            cv2.circle(frame, yellow_center, 5, (0, 0, 255), -1)
+
+    red_cnts = cv2.findContours(red_mask.copy(), cv2.RETR_EXTERNAL,
+                                   cv2.CHAIN_APPROX_SIMPLE)
+    red_cnts = imutils.grab_contours(red_cnts)
+    red_center = None
+
+    yellow_data = np.append(yellow_data, [int(M["m10"] / M["m00"])])
+
+    # only proceed if at least one contour was found
+    if len(red_cnts) > 0:
+        # find the largest contour in the mask, then use
+        # it to compute the minimum enclosing circle and
+        # centroid
+        c = max(red_cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        red_center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+        # only proceed if the radius meets a minimum size
+        if radius > 8:
+            # draw the circle and centroid on the frame,
+            # then update the list of tracked points
+            cv2.circle(frame, (int(x), int(y)), int(radius),
+                       (255, 0, 0), 2)
+            cv2.circle(frame, red_center, 5, (0, 0, 255), -1)
+
 
     # update the points queue
-    pts.appendleft(center)
+    yellow_pts.appendleft(yellow_center)
+    red_pts.appendleft(red_center)
+    red_data = np.append(red_data, [int(M["m10"] / M["m00"])])
 
     # loop over the set of tracked points
-    for i in range(1, len(pts)):
+    for i in range(1, len(yellow_pts)):
         # if either of the tracked points are None, ignore
         # them
-        if pts[i - 1] is None or pts[i] is None:
+        if yellow_pts[i - 1] is None or yellow_pts[i] is None:
             continue
 
         # otherwise, compute the thickness of the line and
         # draw the connecting lines
-        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2)
+        cv2.line(frame, yellow_pts[i - 1], yellow_pts[i], (4, 236, 255), thickness)
+
+    for i in range(1, len(red_pts)):
+        # if either of the tracked points are None, ignore
+        # them
+        if red_pts[i - 1] is None or red_pts[i] is None:
+            continue
+
+        # otherwise, compute the thickness of the line and
+        # draw the connecting lines
+        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2)
+        cv2.line(frame, red_pts[i - 1], red_pts[i], (0, 0, 255), thickness)
 
     # show the frame to our screen
     cv2.imshow("Frame", frame)
@@ -119,3 +169,20 @@ else:
 
 # close all windows
 cv2.destroyAllWindows()
+
+fig, ax = plt.subplots(2,1)
+t_red = np.arange(0, len(red_data), 1)
+t_yellow = np.arange(0, len(yellow_data), 1)
+
+
+ax[0].set(xlabel='frame', ylabel='x position of red dot',
+       title='Red Marker Position')
+
+ax[1].set(xlabel='frame', ylabel='x position of yellow dot',
+       title='Yellow Marker Position')
+
+ax[0].grid()
+ax[1].grid()
+ax[0].plot(t_red, red_data)
+ax[1].plot(t_yellow, yellow_data)
+plt.show()
